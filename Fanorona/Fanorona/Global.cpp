8c3f730613f4f4561a3c9e5309065903a90e38b3
mcgema <65995291+mcgema@users.noreply.github.com>
2022-05-14 19:25:38 -0300

    global variables

---
+++ Fanorona/Fanorona/Global.cpp
@@ -0,0 +1,3 @@
+#include "Global.h"
+int _x_s = -1, _y_s = -1, _player = 1, _pc = -1, _captura = -1;
+int _cx1 = -1, _cy1 = -1, _cx2 = -1, _cy2 = -1;
---
+++ Fanorona/Fanorona/Global.h
@@ -0,0 +1,9 @@
+#pragma once
+// variaveis globais do programa
+// _player - jogador da vez, se 1 é jogador das peças brancas
+// _x_s e y_s representam peça selecionada
+// _captura indica se tem captura ou não (-1 se não e 2 se tem)
+// _cx1, _cy1 , _cx2 , _cy2 representam os dois pontos para captura, quando captura é ambigua
+extern int _x_s, _y_s , _player , _pc , _captura;
+extern int _cx1, _cy1 , _cx2 , _cy2 ;
+
